a124199743effad70f26657bb8720cef1c5be53d
chartorresgg <cedguzman@poligran.edu.co>
2025-03-26 17:37:09 -0500

    Cambios antes de Composite. Ya tengo Builder y Adapter

--- src/main/java/co/poli/edu/cguzman/modelo/Certificacion.java
+++ src/main/java/co/poli/edu/cguzman/modelo/Certificacion.java
@@ -1,7 +1,49 @@
 package co.poli.edu.cguzman.modelo;
 
+/**
+ * Clase Certificacion.
+ * 
+ * Esta clase contiene los atributos y métodos de una certificación.
+ * 
+ * @version 1.0
+ */
 public class Certificacion {
-	
-	
+
+	private String tipoCertificacion;
+	private String entidadCertificadora;
+	private String idCertificacion;
+
+	/**
+	 * Constructor de la clase Certificacion.
+	 * 
+	 * @param tipoCertificacion    Tip o de certificación.
+	 * @param entidadCertificadora Entidad certificadora.
+	 * @param idCertificacion      Identificador de la certificación.
+	 */
+	public Certificacion(String tipoCertificacion, String entidadCertificadora, String idCertificacion) {
+		this.tipoCertificacion = tipoCertificacion;
+		this.entidadCertificadora = entidadCertificadora;
+		this.idCertificacion = idCertificacion;
+	}
+
+	// Getters y Setters
+
+	public String getTipoCertificacion() {
+		return tipoCertificacion;
+	}
+
+	public String getEntidadCertificadora() {
+		return entidadCertificadora;
+	}
+
+	public String getIdCertificacion() {
+		return idCertificacion;
+	}
+
+	@Override
+	public String toString() {
+		return "Certificacion [tipoCertificacion=" + tipoCertificacion + ", entidadCertificadora="
+				+ entidadCertificadora + ", idCertificacion=" + idCertificacion + "]";
+	}
 
 }
--- src/main/java/co/poli/edu/cguzman/modelo/Nequi.java
+++ src/main/java/co/poli/edu/cguzman/modelo/Nequi.java
@@ -1,7 +1,7 @@
 package co.poli.edu.cguzman.modelo;
 
 public class Nequi {
-	
+
 	public void pagar(double monto) {
 		System.out.println("Pago realizado por Nequi: " + monto);
 	}
